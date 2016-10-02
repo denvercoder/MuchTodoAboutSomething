8192ecce9d15ba51afa75a0e6c93a1b4210bd07c
Tim Myers <timmyers1978@gmail.com>
2016-10-01 21:18:51 -0600

    clean up final version 0.1

--- MuchToDoAboutSomething/SecondViewController.swift
+++ MuchToDoAboutSomething/SecondViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,34 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var itemTextField: UITextField!
 
+    @IBAction func add(_ sender: AnyObject) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var items:[String]
+        
+        if let tempItems = itemsObject as? [String] {
+            items = tempItems
+            items.append(itemTextField.text!)
+            
+        } else {
+            
+            items = [itemTextField.text!]
+        }
+        
+        UserDefaults.standard.set(items, forKey: "items")
+        itemTextField.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
